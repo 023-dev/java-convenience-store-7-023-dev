7f638b47804b5097b8578921ca5ee4fbbd01f6c6
023-dev <0_2_3@naver.com>
2024-11-10 00:59:39 +0900

    feat(util): ProductFileReader 구현 클래스 추가
    - 해당 구현체는 products.md을 불러온다.

---
+++ src/main/java/store/util/ProductFileReader.java
@@ -0,0 +1,24 @@
+package store.util;
+
+import store.model.dto.ProductDto;
+
+public class ProductFileReader extends AbstractFileReader<ProductDto> {
+
+    private static final int NAME_INDEX = 0;
+    private static final int PRICE_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
+    private static final int PROMOTION_INDEX = 3;
+
+    @Override
+    protected ProductDto parseLine(String line) {
+        String[] fields = line.split(DELIMITER);
+
+        String name = fields[NAME_INDEX];
+        int price = Integer.parseInt(fields[PRICE_INDEX]);
+        int quantity = Integer.parseInt(fields[QUANTITY_INDEX]);
+        String promotion = fields[PROMOTION_INDEX];
+
+        return new ProductDto(name, price, quantity, promotion);
+    }
+
+}
