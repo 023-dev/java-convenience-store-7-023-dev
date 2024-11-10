5821eaef1f77645baac35f373846507892f35e6c
023-dev <0_2_3@naver.com>
2024-11-10 21:52:30 +0900

    feat(controller): 의존성 추가

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,4 +1,23 @@
 package store.controller;
 
+import store.model.domain.Products;
+import store.model.domain.Promotions;
+import store.view.InputView;
+import store.view.OutputView;
+import store.service.PaymentService;
+
 public class StoreController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Products products;
+    private final Promotions promotions;
+    private final PaymentService paymentService;
+
+    public StoreController(InputView inputView,OutputView outputView, Products products, Promotions promotion, PaymentService paymentService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.products = products;
+        this.promotions = promotion;
+        this.paymentService = paymentService;
+    }
 }
