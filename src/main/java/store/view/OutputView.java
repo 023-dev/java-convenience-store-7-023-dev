dbd45e53a0feb77e6676ff5f9631bcc07f707e1e
023-dev <0_2_3@naver.com>
2024-11-10 18:58:48 +0900

    feat(view): 환영인사 출력 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,5 +1,22 @@
 package store.view;
 
 
+import static store.common.Constants.EMPTY;
+
+import java.util.List;
+import store.model.domain.Product;
+import store.model.domain.Products;
+import store.model.domain.Promotion;
+import store.model.dto.ProductDto;
+
 public class OutputView {
+    private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
+    private static final String PRODUCT_LIST_HEADER = "현재 보유하고 있는 상품입니다.";
+    private static final String PRODUCT_LIST_BODY = "- %s %d원 %s %s";
+    private static final String STOCK_FORMAT = "%d개";
+    private static final String NO_STOCK_MESSAGE = "재고 없음";
+
+    public static void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
 }
