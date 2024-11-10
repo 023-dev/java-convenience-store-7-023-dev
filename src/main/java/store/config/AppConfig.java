be2acfd2596f39f33942353d52f67867d510c033
023-dev <0_2_3@naver.com>
2024-11-10 21:06:57 +0900

    feat(config): StoreController 초기화 메서드 추가

--- src/main/java/store/config/AppConfig.java
+++ src/main/java/store/config/AppConfig.java
@@ -1,4 +1,9 @@
 package store.config;
 
+import store.controller.StoreController;
+
 public class AppConfig {
+    public StoreController storeController() {
+        return new StoreController();
+    }
 }
