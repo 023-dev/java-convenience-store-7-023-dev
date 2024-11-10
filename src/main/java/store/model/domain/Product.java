52ea51449aa2f5d286d56406a3640c0079df980c
023-dev <0_2_3@naver.com>
2024-11-10 11:49:30 +0900

    refactor(domain): Product의 Promotion 수정
    - Promotion에 대한 null값 핸들링을 위해 Optional<Promotion>으로 수정

--- src/main/java/store/model/domain/Product.java
+++ src/main/java/store/model/domain/Product.java
@@ -1,12 +1,14 @@
 package store.model.domain;
 
+import java.util.Optional;
+
 public class Product {
     private final String name;
     private final int price;
     private int stock;
-    private final Promotion promotion;
+    private final Optional<Promotion> promotion;
 
-    public Product(String name, int price, int stock, Promotion promotion) {
+    public Product(String name, int price, int stock, Optional<Promotion> promotion) {
         this.name = name;
         this.price = price;
         this.stock = stock;
