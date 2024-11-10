80f514533bec21e9236551869c2ebdd6267882f0
023-dev <0_2_3@naver.com>
2024-11-10 18:55:13 +0900

    feat(view): 구매할 상품명과 수량 입력 받기 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,4 +1,12 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String INPUT_ITEM_QUANTITY = "구매할 상품명과 수량을 입력해 주세요. (예: [콜라-10],[사이다-3])";
+
+    public String readItemInput() {
+        System.out.println(INPUT_ITEM_QUANTITY);
+        return Console.readLine();
+    }
 }
