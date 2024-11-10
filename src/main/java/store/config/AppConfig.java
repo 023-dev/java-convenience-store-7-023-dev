9ae6c25bfcbb3ed90f1f4807deb7986bd952debb
023-dev <0_2_3@naver.com>
2024-11-10 21:08:55 +0900

    feat(config): promotions() 메서드 추가하여 PromotionLoader로 프로모션 로드

--- src/main/java/store/config/AppConfig.java
+++ src/main/java/store/config/AppConfig.java
@@ -1,9 +1,15 @@
 package store.config;
 
 import store.controller.StoreController;
+import store.model.domain.Promotions;
+import store.util.loader.PromotionLoader;
 
 public class AppConfig {
     public StoreController storeController() {
-        return new StoreController();
+        return new StoreController(promotions());
+    }
+
+    public Promotions promotions() {
+        return new PromotionLoader().loadPromotions();
     }
 }
