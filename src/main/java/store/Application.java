8baf44631cb2a7193ccbcb69de0198edfb5e3979
023-dev <0_2_3@naver.com>
2024-11-10 21:36:21 +0900

    feat: main 메서드에 StoreController 실행 로직 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -7,5 +7,7 @@ public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         StoreController storeController = appConfig.storeController();
+
+        storeController.run();
     }
 }
