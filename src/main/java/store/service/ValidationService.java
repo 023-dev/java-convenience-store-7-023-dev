2aaa114e177c0aa9b0e964a4b898ba39e9aa9690
023-dev <0_2_3@naver.com>
2024-11-11 01:58:20 +0900

    feat(service): 주문 상품 이름이 상품목록 포함  검증 로직 추가

--- src/main/java/store/service/ValidationService.java
+++ src/main/java/store/service/ValidationService.java
@@ -1,34 +1,60 @@
 package store.service;
 
-import static store.common.Constants.DELIMITER;
+import static store.common.Constants.EMPTY;
+import static store.common.Constants.LEFT_BRACKET;
+import static store.common.Constants.QUANTITY_DELIMITER;
+import static store.common.Constants.RIGHT_BRACKET;
 import static store.util.ErrorMessage.*;
 
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ValidationService {
     private static final String ITEM_PATTERN_REGEX = "\\[(\\w+)-(\\d+)]";
     private static final Pattern VALID_ITEM_PATTERN = Pattern.compile(ITEM_PATTERN_REGEX);
 
-    public void validateInput (String input) {
+    public void validateInput (String input, List<String> productNames) {
+        validateNullorBlank(input);
+        validateItemFormat(input);
+        List<String> items = parseItem(input);
+        String productName = items.getFirst();
+        String quantity = items.getLast();
+        validateProductName(productName, productNames);
+        validateQuantity(quantity);
+    }
+
+    private void validateNullorBlank (String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(EMPTY_INPUT.getMessage());
         }
-        List<String> items = parseInput(input);
-        items.forEach(this::validateItemFormat);
     }
 
-    private List<String> parseInput(String input) {
-        return Stream.of(input.split(DELIMITER))
-                .map(String::trim)
-                .collect(Collectors.toList());
+    private List<String> parseItem(String input) {
+        String parseInput =input.replace(LEFT_BRACKET, EMPTY).replace(RIGHT_BRACKET, EMPTY);
+        return List.of(parseInput.split(QUANTITY_DELIMITER));
     }
 
     private void validateItemFormat(String item) {
         if (!VALID_ITEM_PATTERN.matcher(item).matches()) {
             throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
+        }
+    }
+
+    private void validateProductName(String productName, List<String> productNames) {
+        if (!productNames.contains(productName)) {
+            throw new IllegalArgumentException(PRODUCT_NOT_FOUND.getMessage());
+        }
+    }
+
+    private static int validateQuantity(String item) {
+        try {
+            int quantity = Integer.parseInt(item);
+            if (quantity <= 0) {
+                throw new IllegalArgumentException(NON_POSITIVE_QUANTITY.getMessage());
+            }
+            return quantity;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_QUANTITY.getMessage());
         }
     }
 }
