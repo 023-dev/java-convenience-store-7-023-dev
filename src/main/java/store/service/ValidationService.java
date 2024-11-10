26cdaa4a2776b6c917c00737c3f1e66be46728ee
023-dev <0_2_3@naver.com>
2024-11-10 23:34:36 +0900

    feat(service): 입력값을 컬렉션으로 변환하는 parsrInput 메서드 추가

--- src/main/java/store/service/ValidationService.java
+++ src/main/java/store/service/ValidationService.java
@@ -1,4 +1,28 @@
 package store.service;
 
+import static store.common.Constants.DELIMITER;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import store.util.ErrorMessage;
+
 public class ValidationService {
+    private static final String ITEM_PATTERN_REGEX = "\\[(\\w+)-(\\d+)]";
+    private static final Pattern VALID_ITEM_PATTERN = Pattern.compile(ITEM_PATTERN_REGEX);
+
+    public void validateInput (String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+        }
+        List<String> items = parseInput(input);
+    }
+
+    private static List<String> parseInput(String input) {
+        return Stream.of(input.split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
 }
