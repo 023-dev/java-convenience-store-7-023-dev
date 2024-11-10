7da71f5692ba6dd47f8d50b875f7e0fc37c869d9
023-dev <0_2_3@naver.com>
2024-11-10 23:51:56 +0900

    feat(service): VALID_ITEM_PATTERN 정규식을 사용하여 전체 형식 검증 로직 추가

--- src/main/java/store/service/ValidationService.java
+++ src/main/java/store/service/ValidationService.java
@@ -1,12 +1,12 @@
 package store.service;
 
 import static store.common.Constants.DELIMITER;
+import static store.util.ErrorMessage.*;
 
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import store.util.ErrorMessage;
 
 public class ValidationService {
     private static final String ITEM_PATTERN_REGEX = "\\[(\\w+)-(\\d+)]";
@@ -14,15 +14,21 @@ public class ValidationService {
 
     public void validateInput (String input) {
         if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+            throw new IllegalArgumentException(EMPTY_INPUT.getMessage());
         }
         List<String> items = parseInput(input);
+        items.forEach(this::validateItemFormat);
     }
 
-    private static List<String> parseInput(String input) {
+    private List<String> parseInput(String input) {
         return Stream.of(input.split(DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
 
+    private void validateItemFormat(String item) {
+        if (!VALID_ITEM_PATTERN.matcher(item).matches()) {
+            throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
+        }
+    }
 }
