0f538f778add8734b1579b77d8cc2a9966b64baa
023-dev <0_2_3@naver.com>
2024-11-10 19:05:04 +0900

    feat(view): 증정 상품 추가 여부 요청 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,15 +3,16 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_ITEM_QUANTITY = "구매할 상품명과 수량을 입력해 주세요. (예: [콜라-10],[사이다-3])";
+    private static final String ITEM_QUANTITY_PROMPT = "구매할 상품명과 수량을 입력해 주세요. (예: [콜라-10],[사이다-3])";
+    private static final String PROMOTION_PROMPT = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     public String readItemInput() {
-        System.out.println(INPUT_ITEM_QUANTITY);
+        System.out.println(ITEM_QUANTITY_PROMPT);
         return Console.readLine();
     }
 
-    public static boolean readMembershipDiscount() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        return "Y".equalsIgnoreCase(Console.readLine());
+    public String readPromotionOption(String productName) {
+        System.out.printf((PROMOTION_PROMPT) + "\n", productName);
+        return Console.readLine();
     }
 }
